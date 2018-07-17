934189a8f42735e106ae47d9482eab6811a842d8
Amrit <guptaamrit04@gmail.com>
2018-07-17 12:31:09 +0530

    Second Commit

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const app = express();
 
 const path = require('path');
 
-var port = process.env.Port|| 3000;
+var port = process.env.PORT|| 3000;
 
 app.use(express.static(path.join(__dirname,'../public')));
 
